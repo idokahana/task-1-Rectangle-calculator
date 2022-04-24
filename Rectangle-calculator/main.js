9829bd31f48a3fcf0e20bfcc8624e5433d84caa4
idokahana <idoka1233@gmail.com>
2022-04-24 21:24:40 +0300

    task-1-good

--- Rectangle-calculator/main.js
+++ Rectangle-calculator/main.js
@@ -17,35 +17,6 @@ function drowRectangle() {
   calculatorArea();
 }
 
-// function dataObj() {
-//   const x = document.querySelector("#x");
-//   const y = document.querySelector("#y");
-//   const w = document.querySelector("#w");
-//   const h = document.querySelector("#h");
-
-//   let dataObj = {
-//     xValue: x.value,
-//     yValue: y.value,
-//     wValue: w.value,
-//     hValue: h.value,
-//   };
-
-//   localStorage.setItem("properties", JSON.stringify(dataObj));
-// }
-
-// function loadFromLocalStorage() {
-//     const setAddress = JSON.parse(localStorage.getItem('properties'));
-//     streetBox.value = setAddress.street;
-//     houseNumBox.value = setAddress.houseNumber;
-//     cityBox.value = setAddress.city;
-//     stateBox.value = setAddress.state;
-
-//     xValue: x.value,
-//     yValue: y.value,
-//     wValue: w.value,
-//     hValue: h.value,
-// }
-
 function clearCanvas() {
   const myCanvas = document.querySelector("#myCanvas");
   const painter = myCanvas.getContext("2d");
@@ -54,8 +25,9 @@ function clearCanvas() {
 
 function isValid(x, y, w, h) {
   if (
-    x.value + y.value > 300 ||
+    x.value > 300 ||
     x.value < 1 ||
+    y.value > 300 ||
     y.value < 1 ||
     w.value > 300 ||
     w.value < 1 ||
